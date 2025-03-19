0f623731116531455a34cce8809cc0c674f6d4fb
janisbunkss <janisbunkss@inbox.lv>
2025-03-19 16:28:38 +0200

    izveidots programm fails

---
+++ programm.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyCSharpProject
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Game game = new Game();  // Creates an instance of Game
+            game.Start();  // Starts the game loop
+        }
+    }
+}
