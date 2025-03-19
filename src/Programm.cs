d423be473a6df2c0c3ccca6913c8790d352578ec
janisbunkss <janisbunkss@inbox.lv>
2025-03-19 17:03:02 +0200

    remove artifacts from git

---
+++ src/Programm.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Dungeon Game!");
+            // Add your game logic here
+        }
+    }
+}
