e31e2330af9e75301129b36b20aa7c2409212c42
janisbunkss <janisbunkss@inbox.lv>
2025-03-19 16:17:12 +0200

    Faili un folderi sakārtoti atbilstoši ChatGPT ieteikumiem

---
+++ Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System;
+
+
+namespace MyCSharpProject
+{
+    internal class Player
+    {
+        private string name;
+        private int health;
+        private List<string> inventory;
+
+        public Player(string name, int health)
+        {
+            this.name = name;
+            this.health = health;
+            inventory = new List<string>();
+        }
+
+        public void PickUpItem(string item)
+        {
+            inventory.Add(item);
+        }
+
+        public string InventoryContents()
+        {
+            return inventory.Count > 0 ? string.Join(", ", inventory) : "Your inventory is empty.";
+        }
+    }
+}
--- src/obj/Debug/net9.0/MyProject.AssemblyInfo.cs
+++ src/obj/Debug/net9.0/MyProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+486f6dece9fedff1ff6d37a2eb68b6c62e33a04d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
